c2a91b6ac59789b0c195cf31967b0a03e25c68ec
Student <student@Makerss-MacBook-Pro-9.local>
2019-08-06 14:02:40 +0100

    Params Intro

---
+++ README.md
@@ -0,0 +1,4 @@
+
+### Run Automatically 
+command line: shotgun app.rb -p 4567 
+http://localhost:4567
--- app.rb
+++ app.rb
@@ -8,7 +8,15 @@ get '/secret' do
   "Shhhh its a secret!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb :index
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @age = params[:age]
+  @color = params[:color]
   erb :index
 end
